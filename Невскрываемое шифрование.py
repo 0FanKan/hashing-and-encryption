462a91c7894276150fc8796f6a59d118b2508857
FanKan <83346747+0FanKan@users.noreply.github.com>
2022-05-19 17:17:47 +0700

    Add files via upload

---
+++ Невскрываемое шифрование.py
@@ -0,0 +1,25 @@
+from secrets import token_bytes
+from typing import Tuple
+
+def random_key(length):
+    tb = token_bytes(length)
+    return int.from_bytes(tb, "big")
+
+
+def encrypt(original):
+    original_bytes = original.encode()
+    dummy = random_key(len(original_bytes))
+    original_key = int.from_bytes(original_bytes, "big")
+    encrypted = original_key ^ dummy
+    return dummy, encrypted
+
+
+def decrypt(key1, key2):
+    decrypted = key1 ^ key2
+    temp = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
+    return temp.decode()
+
+key1, key2 = encrypt("One Time Pad!")
+print(key2)
+result = decrypt(key1, key2)
+print(result)
